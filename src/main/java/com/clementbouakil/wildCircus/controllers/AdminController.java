45ae1d0c8605935034fbe225d054bd8de8087212
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-10-10 13:51:35 +0200

    adding delete mapping for show and style page admin

--- src/main/java/com/clementbouakil/wildCircus/controllers/AdminController.java
+++ src/main/java/com/clementbouakil/wildCircus/controllers/AdminController.java
@@ -1,13 +1,25 @@
 package com.clementbouakil.wildCircus.controllers;
 
+import java.util.List;
+
+import com.clementbouakil.wildCircus.entities.Spectacle;
+import com.clementbouakil.wildCircus.repositories.SpectacleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    SpectacleRepository spectacleRepository;
+
     @GetMapping("/admin/shows")
-    public String shows() {
+    public String shows(Model model) {
+        List<Spectacle> spectacleList = spectacleRepository.findAll();
+        model.addAttribute("shows", spectacleList);
         return "admin/show";
     }
 
--- src/main/java/com/clementbouakil/wildCircus/controllers/SpectacleController.java
+++ src/main/java/com/clementbouakil/wildCircus/controllers/SpectacleController.java
@@ -7,7 +7,9 @@ import com.clementbouakil.wildCircus.repositories.SpectacleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -24,6 +26,12 @@ public class SpectacleController {
     @PostMapping("/shows")
     public String create(@Valid Spectacle show) {
         spectacleRepository.save(show);
+        return "redirect:/admin/shows";
+    }
+
+    @DeleteMapping("/shows/{id}")
+    public String delete(@PathVariable Long id) {
+        spectacleRepository.deleteById(id);
         return "redirect:/admin/shows";
     }
     
